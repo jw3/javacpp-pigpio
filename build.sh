83253bb01eb1a34ec0c2a965202fc2b9ba777182
John Wass <jwass3@gmail.com>
2017-03-17 21:35:30 -0400

    cp the updated properties file into place

--- build.sh
+++ build.sh
@@ -7,8 +7,9 @@ export JAVA_HOME=/usr/local/java
 git clone https://github.com/bytedeco/javacpp ${javacpp_dir}
 git clone https://github.com/bytedeco/javacpp-presets ${presets_dir}
 
-cp -r "$(pwd)/pigpio" "$presets_dir/pigpio"
-sed -i "s#</modules>#<module>pigpio</module></modules>#g" /tmp/javacpp-presets/pom.xml
+cp ${PWD}/javacpp/properties/* ${javacpp_dir}/src/main/resources/org/bytedeco/javacpp/properties
+cp -r ${PWD}/pigpio ${presets_dir}/pigpio
+sed -i "s#</modules>#<module>pigpio</module></modules>#g" ${presets_dir}/pom.xml
 
 cd ${javacpp_dir}
 mvn install
