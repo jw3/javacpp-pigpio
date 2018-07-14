db9d532e89ae08361c11cb44af7ca0a23ed7d47c
John Wass <jwass3@gmail.com>
2018-07-14 12:23:52 -0400

    move the rm into a clean option

--- build.sh
+++ build.sh
@@ -4,11 +4,16 @@ readonly javacpp_dir=/tmp/javacpp
 readonly presets_dir=/tmp/javacpp-presets
 #export JAVA_HOME=/usr/local/java
 
-git clone https://github.com/bytedeco/javacpp ${javacpp_dir}
-git clone https://github.com/bytedeco/javacpp-presets ${presets_dir}
+if [[ "$1" == "clean" ]]; then
+  rm -rf ${javacpp_dir}
+  rm -rf ${presets_dir}
 
-cp -r "$(pwd)/pigpio" "$presets_dir/pigpio"
-sed -i "s#</modules>#<module>pigpio</module></modules>#g" /tmp/javacpp-presets/pom.xml
+  git clone https://github.com/bytedeco/javacpp ${javacpp_dir}
+  git clone https://github.com/bytedeco/javacpp-presets ${presets_dir}
+
+  cp -r "$(pwd)/pigpio" "$presets_dir/pigpio"
+  sed -i "s#</modules>#<module>pigpio</module></modules>#g" /tmp/javacpp-presets/pom.xml
+fi
 
 cd ${javacpp_dir}
 mvn install
