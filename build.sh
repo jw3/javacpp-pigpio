894cc911220085b611bf000b6a0beb6b235841e2
John Wass <jwass3@gmail.com>
2018-07-19 02:26:18 -0400

    fix up version replacement in pom

--- build.sh
+++ build.sh
@@ -15,6 +15,8 @@ readonly javacpp_ver="${JAVACPP_VER:-1.4.1}"
 export PIGPIO_REPO="${PIGPIO_REPO:-https://github.com/joan2937/pigpio}"
 export PIGPIO_MAJOR_VERSION=67
 
+readonly library_ver="$PIGPIO_MAJOR_VERSION.$version-$javacpp_ver"
+
 _echoerr() { if [[ ${QUIET} -ne 1 ]]; then echo "[debug]: $@" 1>&2; fi }
 _checkerr() { ec=${1}; if [[ ${ec} -ne 0 ]]; then _echoerr "$2 failed with error code $ec"; exit ${ec}; fi }
 
@@ -41,4 +43,6 @@ _checkerr $? "presets checkout $javacpp_ver"
 cp -r "$sourcedir/pigpio" "$presets_dir/pigpio"
 sed -i "s#</modules>#<module>pigpio</module></modules>#g" "$presets_dir/pom.xml"
 sed -i "s#__JAVACPP_VER__#${javacpp_ver}#g" "$presets_dir/pigpio/pom.xml"
+sed -i "s#__LIBRARY_VER__#${library_ver}#g" "$presets_dir/pigpio/pom.xml"
+
 mvn install --projects .,pigpio -Dpigpio.major="$PIGPIO_MAJOR_VERSION" -Dlibrary.version="$version"
